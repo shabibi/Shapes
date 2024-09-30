fb4fc4cc011a3018cb6d23a38012a65fb6be4310
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-30 15:38:45 +0400

    Adding Shapes abstract class and square class.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public abstract class Shapes
+    {
+        protected double length;
+        protected double whidth;
+
+        public abstract double area();
+    }
+
+    public class square : Shapes
+    {
+        public square(double length)
+        {
+            this.length = length;
+        }
+        public override double area()
+        {
+            return length*length;
+        }
+    }
+}
